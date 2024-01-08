a1ff15d522126bd185bd3a66e9a002c8803b9914
Every Packet Matters Bear <epmb@realmrei.com>
2024-01-08 00:17:44 -0700

    v0.9.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='gentooimgr',
     description='Gentoo image generator and install automator tool',
-    version='0.2.0',
+    version='0.9.1',
     include_package_data=True,
     packages=find_packages()
 )
