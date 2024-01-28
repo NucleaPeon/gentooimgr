7b6450d95f85df41355ccf43631d369ed31dc615
Every Packet Matters Bear <epmb@realmrei.com>
2024-01-27 22:24:42 -0700

    Bumping patch version, removing custom repo sync-uri

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='gentooimgr',
     description='Gentoo image generator and install automator tool',
-    version='0.9.1',
+    version='0.10.1',
     include_package_data=True,
     packages=find_packages()
 )
